2774c23a4a74c47706fb71de3354944bb0d05db0
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-11 17:13:47 +0100

    Add waveforms and play sound

--- src/client/src/components/Track.component.jsx
+++ src/client/src/components/Track.component.jsx
@@ -1,10 +1,27 @@
-import { WaveSurfer } from 'wavesurfer-react';
+import { useEffect, useRef } from 'react';
+
+import WaveSurfer from 'wavesurfer.js';
 
 export default function Track() {
+  const waveformRef = useRef();
+
+  useEffect(() => {
+    if (waveformRef.current) {
+      const wavesurfer = WaveSurfer.create({
+        container: waveformRef.current,
+      });
+      wavesurfer.on('ready', function () {
+        // wavesurfer.play();
+      });
+      wavesurfer.load(
+        'https://actions.google.com/sounds/v1/science_fiction/creature_distortion_white_noise.ogg'
+      );
+    }
+  }, []);
+
   return (
     <div>
-      <div>Track component work!</div>
-      <div></div>
+      <div className="w-64" ref={waveformRef}></div>
     </div>
   );
 }
