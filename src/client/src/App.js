0740c2042ba69ad04f8ab28831aec394bab0e28f
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-11 15:38:58 +0100

    Add separation lines

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,7 +2,7 @@ import Profile from './components/Profile.component.jsx';
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-neutral-800">
+    <div className="h-screen w-screen bg-neutral-900">
       <Profile />
     </div>
   );
--- src/client/src/components/Profile.component.jsx
+++ src/client/src/components/Profile.component.jsx
@@ -1,7 +1,7 @@
 export default function Profile () {
   return (
     <div className="h-screen w-screen">
-      <div className="h-full w-full flex flex-col justify-center items-center p-9">
+      <div className="h-full w-full flex flex-col justify-center items-center p-6">
           <div className="flex flex-col justify-center items-center">
           <img
             className="w-48 h-48 rounded"
@@ -11,14 +11,14 @@ export default function Profile () {
           <h1 className="text-white text-2xl mt-7 mb-1">Mateo Presa</h1>
           <p className="text-neutral-400">@mateopresa</p>
         </div>
-        <div className="my-3"></div>
+        <hr class="w-full border-neutral-800 my-7" />
         <div>
-          <p className="text-xs text-white text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id neque at nulla suscipit dapibus.</p>
+          <p className="text-xs text-neutral-400 text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id neque at nulla suscipit dapibus.</p>
         </div>
-        <div className="my-3"></div>
-          <div className="w-full">
-            <h2 className="text-white text-xl text-left">Tracks</h2>
-          </div>
+        <hr class="w-full border-neutral-800 my-7" />
+        <div className="w-full">
+          <h2 className="text-white text-xl text-left">Tracks</h2>
+        </div>
 
       </div>
     </div>
