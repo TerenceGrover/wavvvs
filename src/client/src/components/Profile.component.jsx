c5193e98ad284e99664e7ab5e6615670ca6c3c77
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-11 16:28:09 +0100

    Add semantic html. Add track component to git

--- src/client/src/components/Profile.component.jsx
+++ src/client/src/components/Profile.component.jsx
@@ -4,7 +4,7 @@ export default function Profile() {
   return (
     <div className="h-screen w-screen">
       <div className="h-full w-full flex flex-col justify-center items-center p-6">
-        <div className="flex flex-col justify-center items-center">
+        <section className="flex flex-col justify-center items-center">
           <img
             className="w-48 h-48 rounded"
             src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2563.jpg?w=2000"
@@ -12,19 +12,21 @@ export default function Profile() {
           />
           <h1 className="text-white text-2xl mt-7 mb-1">Mateo Presa</h1>
           <p className="text-neutral-400">@mateopresa</p>
-        </div>
+        </section>
         <hr className="w-full border-neutral-800 my-6" />
-        <div>
+        <section>
           <p className="text-xs text-neutral-400 text-left">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             id neque at nulla suscipit dapibus.
           </p>
-        </div>
+        </section>
         <hr className="w-full border-neutral-800 my-6" />
-        <div className="w-full">
-          <h2 className="text-white text-xl text-left">Tracks</h2>
-        </div>
-        <Track />
+        <section>
+          <div className="w-full">
+            <h2 className="text-white text-xl text-left">Tracks</h2>
+          </div>
+          <Track />
+        </section>
       </div>
     </div>
   );
---
+++ src/client/src/components/Track.component.jsx
@@ -0,0 +1,10 @@
+import { WaveSurfer } from 'wavesurfer-react';
+
+export default function Track() {
+  return (
+    <div>
+      <div>Track component work!</div>
+      <div></div>
+    </div>
+  );
+}
