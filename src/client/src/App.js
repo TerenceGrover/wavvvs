b70064910c303737fb99667458e8a4a39e605ec0
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-11 16:24:39 +0100

    Add layout. Import wavesurfer-react

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -3,7 +3,11 @@ import Profile from './components/Profile.component.jsx';
 function App() {
   return (
     <div className="h-screen w-screen bg-neutral-900">
-      <Profile />
+      <header></header>
+      <main>
+        <Profile />
+      </main>
+      <footer></footer>
     </div>
   );
 }
--- src/client/src/components/Profile.component.jsx
+++ src/client/src/components/Profile.component.jsx
@@ -1,8 +1,10 @@
-export default function Profile () {
+import Track from './Track.component.jsx';
+
+export default function Profile() {
   return (
     <div className="h-screen w-screen">
       <div className="h-full w-full flex flex-col justify-center items-center p-6">
-          <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
           <img
             className="w-48 h-48 rounded"
             src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2563.jpg?w=2000"
@@ -11,16 +13,19 @@ export default function Profile () {
           <h1 className="text-white text-2xl mt-7 mb-1">Mateo Presa</h1>
           <p className="text-neutral-400">@mateopresa</p>
         </div>
-        <hr class="w-full border-neutral-800 my-7" />
+        <hr className="w-full border-neutral-800 my-6" />
         <div>
-          <p className="text-xs text-neutral-400 text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id neque at nulla suscipit dapibus.</p>
+          <p className="text-xs text-neutral-400 text-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            id neque at nulla suscipit dapibus.
+          </p>
         </div>
-        <hr class="w-full border-neutral-800 my-7" />
+        <hr className="w-full border-neutral-800 my-6" />
         <div className="w-full">
           <h2 className="text-white text-xl text-left">Tracks</h2>
         </div>
-
+        <Track />
       </div>
     </div>
-  )
+  );
 }
