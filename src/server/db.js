7a2962505f4f236f915dcadbff7369385335e79a
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-13 17:59:36 +0100

    Create simplified version model. Add seed script

--- src/server/db.js
+++ src/server/db.js
@@ -1,10 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 try {
+  mongoose.set('strictQuery', true);
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  console.log('Connected to the DB');
 } catch (error) {
   console.log('Error connecting to DB');
-  console.log(error)
+  console.log(error);
 }
 
 export default mongoose;
--- src/server/models/models.js
+++ src/server/models/models.js
@@ -1,24 +1,17 @@
 import mongoose from '../db.js';
 
 const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   name: String,
   user_name: String,
   email: String,
   password: String,
   bio: String,
   profile_pic_path: String,
-  tracks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Track' }],
-});
-
-const trackSchema = mongoose.Schema({
-  uploaded_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  filename: String,
-  name: String,
-  size: Number,
+  track0: String,
+  track1: String,
+  track2: String,
 });
 
 const User = mongoose.model('User', userSchema);
-const Track = mongoose.model('Track', trackSchema);
 
-export { User, Track };
+export { User };
