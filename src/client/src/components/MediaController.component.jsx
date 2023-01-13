32b49e03da1959d8dcd876ba179f1104c00ac161
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-13 14:28:08 +0100

    Resize buttons

--- src/client/src/components/MediaController.component.jsx
+++ src/client/src/components/MediaController.component.jsx
@@ -49,30 +49,30 @@ export default function MediaController({ activeTrack, setTrackList }) {
         <div className="h-full flex justify-center items-center align-center">
           <div className=" flex w-96 h-full justify-between items-center">
             <div className="flex w-28 justify-between items-center">
-              <TbPlayerSkipBack className="text-neutral-200 h-6 w-6 mr-3" />
+              <TbPlayerSkipBack className="text-neutral-200 h-5 w-5 mr-3" />
               {!activeTrack?.isPlaying ? (
                 <IoPlay
                   onClick={handlePlayClick}
-                  className="text-neutral-200 h-6 w-6 mr-3"
+                  className="text-neutral-200 h-5 w-5 mr-3"
                 />
               ) : (
                 <IoStop
                   onClick={handlePlayClick}
-                  className="text-neutral-200 h-6 w-6 mr-3"
+                  className="text-neutral-200 h-5 w-5 mr-3"
                 />
               )}
-              <TbPlayerSkipForward className="text-neutral-200 h-6 w-6 mr-3" />
+              <TbPlayerSkipForward className="text-neutral-200 h-5 w-5 mr-3" />
             </div>
             <div className="flex w-14 justify-between items-center">
               {isMuted ? (
                 <FiVolumeX
                   onClick={handleMuteClick}
-                  className="text-neutral-200 h-6 w-7 ml-3"
+                  className="text-neutral-200 h-5 w-7 ml-3"
                 />
               ) : (
                 <FiVolume2
                   onClick={handleMuteClick}
-                  className="text-neutral-200 h-6 w-7 ml-3"
+                  className="text-neutral-200 h-5 w-7 ml-3"
                 />
               )}
             </div>
