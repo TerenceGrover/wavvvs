493dc31b1b800afe189d8a0d482e9475123d850a
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-14 18:30:32 +0100

    Change button to Link

--- src/client/src/components/LoginButton.component.jsx
+++ src/client/src/components/LoginButton.component.jsx
@@ -1,14 +1,8 @@
-import { useAuth0 } from '@auth0/auth0-react';
-
+import { Link } from 'react-router-dom';
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
   return (
-    <button
-      className="transition ease-in duration-200 w-20 rounded bg-neutral-800 py-2 px-1 mt-2 text-xs text-white hover:bg-neutral-700"
-      onClick={() => loginWithRedirect()}
-    >
-      Log In
+    <button className="transition ease-in duration-200 w-20 rounded bg-neutral-800 py-2 px-1 mt-2 text-xs text-white hover:bg-neutral-700">
+      <Link to={'/mateopresa'}> Log In</Link>
     </button>
   );
 };
