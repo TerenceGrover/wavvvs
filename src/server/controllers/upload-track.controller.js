4cd02bef06fe3af71abda6004f27b7f8d61913af
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-14 19:40:24 +0100

    Change schemas. This is a mess.

--- src/server/controllers/upload-track.controller.js
+++ src/server/controllers/upload-track.controller.js
@@ -1,20 +1,33 @@
 import { User } from '../models/models.js';
 
 export default async function uploadTrack(req, res) {
+  console.log('im here in uploadTrack controller');
   const { username } = req.params;
   const { originalname, filename } = req.file;
   // todo: save path to track on user Model in database
   const user = await User.findOne({ user: username });
-  if (!user.track0) {
+  console.log({ user });
+  if (user.track0 === '') {
     await User.findOneAndUpdate({ user: username }, { track0: filename });
+    sendUserBackToClient(username, res);
   }
-  if (!user.track1) {
+  if (user.track1 === '') {
     await User.findOneAndUpdate({ user: username }, { track1: filename });
+    sendUserBackToClient(username, res);
   }
-  if (!user.track2) {
+  if (user.track2 === '') {
     await User.findOneAndUpdate({ user: username }, { track2: filename });
+    sendUserBackToClient(username);
   }
+  // const updatedUser = await User.findOne({ user: username });
+  // console.log({ updatedUser });
+  // res.status(200);
+  // res.send(updatedUser);
+}
+
+const sendUserBackToClient = async (username, res) => {
   const updatedUser = await User.findOne({ user: username });
+  console.log({ updatedUser });
   res.status(200);
   res.send(updatedUser);
-}
+};
