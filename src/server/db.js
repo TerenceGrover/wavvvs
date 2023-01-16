d7d4097c12ff350c0066b80b0c79fc3ac1581f45
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-16 17:49:48 +0100

    Working MVP come here if you break everything

--- src/server/db.js
+++ src/server/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 try {
   mongoose.set('strictQuery', true);
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/wavvvs');
   console.log('Connected to the DB');
 } catch (error) {
   console.log('Error connecting to DB');
