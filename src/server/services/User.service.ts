c2f297f679dd15d2eaadd7b918a705fd458fce0b
Al366io <alessio.nannipieri0@gmail.com>
2023-01-20 15:59:59 +0100

    feat: set up auth with JWT and handle new users

--- src/server/services/User.service.ts
+++ src/server/services/User.service.ts
@@ -1,7 +1,9 @@
 import { DocumentDefinition } from 'mongoose';
+import jwt from 'jsonwebtoken';
 import { User, IUser } from '../models/models';
 import bcrypt from 'bcrypt';
 const saltRounds = 8;
+const {SECRET_KEY} = process.env;
 
 // this works. Maybe sanitize inputs.
 export async function register(user: DocumentDefinition<IUser>) {
@@ -9,10 +11,18 @@ export async function register(user: DocumentDefinition<IUser>) {
     user.password = await bcrypt.hash(user.password, saltRounds);
   }
   try {
-    const createdUser = await User.create(user);
+    // setting isNew to true so next time he logs in he will be automatically redirected to modify user page.
+    const userToInsert = {
+      isNew: true,
+      name: user.name,
+      email: user.email,
+      bio: '',
+      profile_pic_path: user.profile_pic_path
+    }
+    const createdUser = await User.create(userToInsert);
     if(createdUser) {
       createdUser.toJSON();
-      delete createdUser.password
+      createdUser.password = '';
       return createdUser;
     }
   } catch (error) {
@@ -29,7 +39,14 @@ export async function login(user: DocumentDefinition<IUser>) {
     }
     const isMatch = bcrypt.compareSync(user.password!, foundUser.password!);
     if (isMatch) {
-      return foundUser;
+      const token = jwt.sign({ _id: foundUser._id?.toString(), name: foundUser.name }, SECRET_KEY!, {
+        expiresIn: '2 days',
+      });
+      
+      // change isNew to false since now it has logged in one time and from now on he will be redirected to dashboard
+      foundUser.isNew = false;
+      foundUser.save();
+      return { user: { name: foundUser.name, _id: foundUser._id }, token: token };
     } else {
       throw new Error('Password is not correct');
     }
