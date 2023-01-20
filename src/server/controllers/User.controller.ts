f7df93d676b1b11e1c4f0f6a595451e0729a9b8f
Al366io <alessio.nannipieri0@gmail.com>
2023-01-20 18:55:25 +0100

    feat: added getUser endpoint (jwt)

--- src/server/controllers/User.controller.ts
+++ src/server/controllers/User.controller.ts
@@ -2,22 +2,29 @@ import { User, IUser } from '../models/models.js';
 import { Request, Response } from 'express';
 import * as userServices from '../services/User.service';
 import { getErrorMessage } from '../utils/error.util';
+import jwt, {Secret} from 'jsonwebtoken';
+const { SECRET_KEY } = process.env;
 
 const getUser = async (req: Request, res: Response) => {
   try {
-    const username: string = req.body.username;
-    const user: IUser | null = await User.findOne({ user: username });
-    if (user) {
-      const userToSend = {
-        name: user.name,
-        email: user.email,
-        bio: user.bio,
-        profile_pic_path: user.profile_pic_path,
-      };
-      res.status(200).send(userToSend);
-    }
-    // if user exists, return 200 and return the user, otherwise 404
-    res.sendStatus(404);
+
+    if (req.headers && req.headers.authorization) {
+      let authorization = req.headers.authorization.split(' ')[1],
+          decoded:any;
+      try {
+          decoded = jwt.verify(authorization, SECRET_KEY!);
+      } catch (e) {
+          return res.status(401).send('unauthorized');
+      }
+      const id = decoded.id;
+      // Fetch the user by id 
+      User.findOne({_id: id}).then(function(user){
+          // Do something with the user
+          user!.password = '';
+          return res.status(200).send(user);
+      });
+  }
+  return res.send(500);
   } catch (error) {
     console.log({ error });
     res.status(500).send({ error: getErrorMessage(error) });
