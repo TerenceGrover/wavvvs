0ef6582c899be9f612cd0668da53b2314133106a
TerenceGrover <terencegrover@gmail.com>
2023-01-21 19:39:42 +0100

    Added loader in comment on profile so that all loads after pic

--- src/client/src/components/Header.component.tsx
+++ src/client/src/components/Header.component.tsx
@@ -10,10 +10,13 @@ const staticPicUrl = `${REACT_APP_BACKEND_HOST}/pics/mateo_pic.jpeg`;
 export default function Header( props: {currentUser: CurrentUser}) {
 
   const [search, setSearch] = React.useState(false);
+  const [loading , setLoading] = React.useState(true);
 
   return (
     <div className="fixed top-0 left-0 right-0 z-10 max-w-92">
       <header className="flex justify-center h-14 text-white bg-neutral-800">
+        {/* {!loading
+          && */}
         <div className="w-96 py-3.5 flex justify-between lg:max-w-xl lg:w-full">
           <div className="flex w-26 h-full justify-between items-center content-center">
             <AiOutlineMenu className="text-neutral-200 h-6 w-6 mr-3 hover:text-neutral-400 ease-in transition duration-100 cursor-pointer" />
@@ -42,10 +45,12 @@ export default function Header( props: {currentUser: CurrentUser}) {
               className="w-6 h-6 ml-3 rounded-full"
               src={props.currentUser.profile_pic_path ? props.currentUser.profile_pic_path : 'https://i.imgur.com/6VBx3io.png'}
               alt="User profile"
+              onLoadStart={() => setLoading(false)}
             />
             </Link>
           </div>
         </div>
+        {/* } */}
       </header>
     </div>
   );
--- src/client/src/pages/ProfilePage.tsx
+++ src/client/src/pages/ProfilePage.tsx
@@ -7,14 +7,8 @@ import Logo from "../components/Logo.component";
 export default function ProfilePage (props : {currentUser: CurrentUser, setCurrentUser: React.Dispatch<React.SetStateAction<CurrentUser | undefined>>}) {
   
   // setup a loader to wait for the currentUser to be set
-  const [loading, setLoading] = React.useState(true);
   
   return (
-    loading ? 
-    <main className="w-screen h-screen flex flex-col justify-center items-center">
-      <Logo/>
-    </main>
-    :
     <div id="profile-container">
       <Header currentUser={props.currentUser} />
         <Profile currentUser={props.currentUser} setCurrentUser={props.setCurrentUser} />
