e8fa905cc9c93a836f174a22149d078e49359d39
Al366io <alessio.nannipieri0@gmail.com>
2023-01-25 11:17:41 +0100

    feat: added id to user in getAnotherUser

--- src/server/tests/global.test.ts
+++ src/server/tests/global.test.ts
@@ -3,6 +3,7 @@ import * as http from 'http';
 import app from '../index';
 import cloudinary from 'cloudinary';
 import * as mypath from 'path';
+import { convertToObject } from 'typescript';
 const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
 
 let server: http.Server;
@@ -82,7 +83,7 @@ describe('login & register functionalities', () => {
 });
 
 describe('interact with user functionalities', () => {
-  let token: string;
+  let tokenOfUser1: string;
   beforeAll(async () => {
     await startServer();
 
@@ -95,16 +96,22 @@ describe('interact with user functionalities', () => {
       email: 'ale@gmail.com',
       password: '12345689',
     });
-    token = res.body.token;
+    tokenOfUser1 = res.body.token;
   });
 
   afterAll(async () => {
     // delete user created in beforeEach
     await request(app)
       .delete('/user')
-      .set('Authorization', `Bearer ${token}`)
+      .set('Authorization', `Bearer ${tokenOfUser1}`)
       .send({
         password: '12345689',
+      });
+    await request(app)
+      .delete('/user')
+      .set('Authorization', `Bearer ${tokenOfUser2}`)
+      .send({
+        password: '123456789',
       });
     await stopServer();
   });
@@ -118,7 +125,7 @@ describe('interact with user functionalities', () => {
         profile_pic_path: 'path',
         isPrivate: false,
       })
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
     expect(res.status).toBe(204);
   });
 
@@ -145,7 +152,7 @@ describe('interact with user functionalities', () => {
       .send({
         url: path,
       })
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
     expect(res.status).toBe(204);
   });
 
@@ -154,7 +161,7 @@ describe('interact with user functionalities', () => {
   it('should get the user profile', async () => {
     const res = await request(app)
       .get('/user')
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
     expect(res.status).toBe(200);
     expect(res.body.name).toBe('ale');
 
@@ -162,56 +169,46 @@ describe('interact with user functionalities', () => {
     arrOfTracks = res.body.tracks;
   });
 
-  it('should delete a track for the user', async () => {
-    const res = await request(app)
-      .delete('/track')
-      .send({
-        id: arrOfTracks[0]._id,
-      })
-      .set('Authorization', `Bearer ${token}`);
-    expect(res.status).toBe(204);
-  });
-
   it('should automatically delete a track if its user is deleted', async () => {
     // TODO : implement this test
   });
 
-  it('should delete a track after 24 hours', async () => {
-    // first post a track
-    const filePath = mypath.join(
-      __dirname,
-      '..',
-      'tests',
-      'mocks',
-      'rickroll.m4a'
-    );
-    cloudinary.v2.config({
-      cloud_name: CLOUD_NAME,
-      api_key: API_KEY,
-      api_secret: API_SECRET,
-    });
-    const upload = await cloudinary.v2.uploader.upload(filePath, {
-      resource_type: 'video',
-    });
-    const path = upload.secure_url;
-    console.log(path);
-    // we can't wait 24 hours in a test so we will just post to a test route that injects a date in the past
-    const res = await request(app)
-      .post('/test/user/tracks')
-      .send({ url: path })
-      .set('Authorization', `Bearer ${token}`);
-    expect(res.status).toBe(204);
+  // it('should delete a track after 24 hours', async () => {
+  //   // first post a track
+  //   const filePath = mypath.join(
+  //     __dirname,
+  //     '..',
+  //     'tests',
+  //     'mocks',
+  //     'rickroll.m4a'
+  //   );
+  //   cloudinary.v2.config({
+  //     cloud_name: CLOUD_NAME,
+  //     api_key: API_KEY,
+  //     api_secret: API_SECRET,
+  //   });
+  //   const upload = await cloudinary.v2.uploader.upload(filePath, {
+  //     resource_type: 'video',
+  //   });
+  //   const path = upload.secure_url;
+  //   console.log(path);
+  //   // we can't wait 24 hours in a test so we will just post to a test route that injects a date in the past
+  //   const res = await request(app)
+  //     .post('/test/user/tracks')
+  //     .send({ url: path })
+  //     .set('Authorization', `Bearer ${tokenOfUser1}`);
+  //   expect(res.status).toBe(204);
 
-    // we wait 2 second to be sure that the track is deleted
-    await new Promise((resolve) => {
-      setTimeout(resolve, 10000);
-    });
-    const res2 = await request(app)
-      .get('/user')
-      .set('Authorization', `Bearer ${token}`);
-    expect(res2.status).toBe(200);
-    expect(res2.body.tracks.length).toBe(0);
-  }, 20000);
+  //   // we wait 10 second to be sure that the track is deleted
+  //   await new Promise((resolve) => {
+  //     setTimeout(resolve, 10000);
+  //   });
+  //   const res2 = await request(app)
+  //     .get('/user')
+  //     .set('Authorization', `Bearer ${tokenOfUser1}`);
+  //   expect(res2.status).toBe(200);
+  //   expect(res2.body.tracks.length).toBe(0);
+  // }, 20000);
 
   it('should throw an error if user tries to follow himself', async () => {
     // get user calling the /me endpoint
@@ -220,11 +217,11 @@ describe('interact with user functionalities', () => {
       .send({
         id: id,
       })
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
     expect(res.status).toBe(400);
   });
 
-  let tokenOfUser2 = ''
+  let tokenOfUser2 = '';
   it('should follow a new user', async () => {
     // register a new user
     await request(app).post('/register').send({
@@ -243,14 +240,14 @@ describe('interact with user functionalities', () => {
       .get('/user')
       .set('Authorization', `Bearer ${token2}`);
     const id2 = res2.body._id;
-    tokenOfUser2=id2;
+    tokenOfUser2 = id2;
     // follow the new user with the other user
     const res3 = await request(app)
       .put('/user/follow')
       .send({
         id: id2,
       })
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
     expect(res3.status).toBe(204);
     // get new user profile to see if the other user is in the followers array
     const res4 = await request(app)
@@ -273,14 +270,33 @@ describe('interact with user functionalities', () => {
     const res = await request(app)
       .put('/track/like')
       .send({
-        trackId: arrOfTracks[0]._id
+        trackId: arrOfTracks[0]._id,
       })
       .set('Authorization', `Bearer ${tokenOfUser2}`);
+    expect(res.status).toBe(204);
+
+    const res2 = await request(app)
+      .get('/user')
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
+    expect(res2.status).toBe(200);
+    expect(res2.body.name).toBe('ale');
+    expect(res2.body.tracks[0].likes).toBe(1);
   });
 
   it('should delete likes and following of user when user gets deleted', async () => {
     // delete the user
     // expect ale followers to be 0
     // expect ale's song lieks to be 0
+  });
+
+  // delete ale's track
+  it('should delete a track for the user', async () => {
+    const res = await request(app)
+      .delete('/track')
+      .send({
+        id: arrOfTracks[0]._id,
+      })
+      .set('Authorization', `Bearer ${tokenOfUser1}`);
+    expect(res.status).toBe(204);
   });
 });
