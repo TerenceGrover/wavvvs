5a2a5a663d13bc3e760a0e0c5cf214931b8714dd
TerenceGrover <terencegrover@gmail.com>
2023-01-25 17:11:12 +0100

    feat : linked the mobile context and added a menu for mobile

--- src/client/src/components/Header.component.tsx
+++ src/client/src/components/Header.component.tsx
@@ -2,22 +2,46 @@ import React from 'react';
 import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import Logo from './Logo.component';
+import Menu from './Menu.component';
 import { Context } from '../Utils/Context';
 
 export default function Header() {
   const { currentUser, mobile } = React.useContext(Context);
 
   const [search, setSearch] = React.useState(false);
+  const [menu, setMenu] = React.useState(false);
+
+  const handleClick = (e : any) => {
+    const target = e.target as HTMLElement;
+    if (target.id === 'hamburger-menu' && !menu) {
+      target.classList.add('transform');
+      target.classList.add('rotate-90');
+    } else {
+      target.classList.remove('transform');
+      target.classList.remove('rotate-90');
+    }
+    setMenu(!menu);
+  };
 
   return (
     <div className="fixed top-0 left-0 right-0 z-10 max-w-92">
-      <header className={`${mobile && 'px-4'} flex justify-center h-14 text-white bg-neutral-800`}>
+      <header id='header' className={`${mobile && 'px-4'} flex justify-center h-14 text-white bg-neutral-800`}>
+      {
+        menu
+        &&
+        <Menu />
+      }
         <div className="w-96 py-3.5 flex justify-between lg:max-w-xl lg:w-full">
           <div className="flex w-26 h-full justify-between items-center content-center">
+            {
+              mobile
+              &&
             <AiOutlineMenu
-              id="hamburger-menu"
-              className="text-neutral-200 h-6 w-6 mr-3 hover:text-neutral-400 ease-in transition duration-100 cursor-pointer"
+            id="hamburger-menu"
+            className="text-neutral-200 h-6 w-6 mr-3 hover:text-neutral-400 ease-in transition duration-100 cursor-pointer"
+            onClick={e => handleClick(e)}
             />
+          }
             <div className="mb-1">
               <Link to="/">
                 <Logo />
--- src/client/src/components/ProfileCard.component.tsx
+++ src/client/src/components/ProfileCard.component.tsx
@@ -7,9 +7,9 @@ export default function ProfileCard() {
   const { selectedUser } = React.useContext(Context);
 
   return (
-    <div className='flex flex-col max-w-[275px] min-w-[175px] w-[70%] h-[70%] bg-neutral-800 shadow-sm shadow-black self-center justify-around items-center py-[2.5%] rounded-xl'>
+    <div className='flex flex-col max-w-[275px] min-w-[175px] w-[70%] h-[60%] bg-neutral-800 shadow-sm shadow-black self-center justify-around items-center py-[2.5%] rounded-xl'>
       <div className='flex flex-col items-center'>
-        <img className='w-24 h-24 rounded-full' src={selectedUser.profile_pic_path} alt='Profile' />
+        <img className='w-24 h-24 rounded-2xl' src={selectedUser.profile_pic_path} alt='Profile' />
         <span className='text-neutral-200 text-xl'>{'@' + selectedUser.username}</span>
         <span className='text-neutral-200 text-xl'>{selectedUser.name}</span>
       </div>
